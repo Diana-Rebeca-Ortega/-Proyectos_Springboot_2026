29b6d7e811a3a0ee540dcd4e0f5e2839a87eacdc
Diana Rebeca Ortega <diana.os@tecjerez>
2026-06-03 09:46:36 -0600

    Se agregaron los archivos para materio servicio y repositorio

---
+++ Proyecto_ABCC_MySQL/src/main/java/com/example/Proyecto_ABCC_MySQL/servicios/MateriaService.java
@@ -0,0 +1,18 @@
+package com.example.Proyecto_ABCC_MySQL.servicios;
+
+import com.example.Proyecto_ABCC_MySQL.models.Materia;
+import com.example.Proyecto_ABCC_MySQL.repositories.MateriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class MateriaService {
+    @Autowired
+    private MateriaRepository materiaRepository;
+
+    public List<Materia> listarTodas() {
+        return materiaRepository.findAll();
+    }
+    
+}
