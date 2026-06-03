0346a0193fd6d046b51dc1be6a34c576f9959010
Diana Rebeca Ortega <diana.os@tecjerez>
2026-06-03 10:03:10 -0600

    Se añadio el index.html

--- Proyecto_ABCC_MySQL/src/main/java/com/example/Proyecto_ABCC_MySQL/controllers/ViewController.java
+++ Proyecto_ABCC_MySQL/src/main/java/com/example/Proyecto_ABCC_MySQL/controllers/ViewController.java
@@ -1,13 +1,23 @@
 package com.example.Proyecto_ABCC_MySQL.controllers;
 
+import com.example.Proyecto_ABCC_MySQL.servicios.AlumnoService; // Ajusta según tus servicios
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ViewController {
-    
+
+    @Autowired 
+    private AlumnoService alumnoService;
+    // @Autowired private MateriaService materiaService; 
+    // @Autowired private ActividadService actividadService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        // Aquí pasamos los datos a la vista
+        model.addAttribute("listaAlumnos", alumnoService.findAlumnos());
         return "index";
     }
 }
